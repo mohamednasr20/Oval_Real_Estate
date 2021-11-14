e62fd7c4b9ae3f9e54e47daab85dc3a63cf12637
mohamednasr20 <mohamednasr8687@gmail.com>
2021-11-13 23:37:00 -0500

    add react hook form to the auth

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -4,6 +4,7 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { useSelector, useDispatch } from 'react-redux';
+import { useForm, Controller } from 'react-hook-form';
 import { handleShowAuthModal } from '../../actions/globalState';
 import useStyles from './styles';
 
@@ -12,6 +13,11 @@ const Auth = () => {
   const dispatch = useDispatch();
   const showAuth = useSelector((state) => state.globalState.showAuth);
   const [signIn, setSignIn] = useState(false);
+  const { handleSubmit, control } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
     <div className={classes.root}>
@@ -21,53 +27,138 @@ const Auth = () => {
         aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description"
       >
-        <form className={classes.paper}>
+        <form className={classes.paper} onSubmit={handleSubmit(onSubmit)}>
           <Typography gutterBottom variant="h5">
             {signIn ? 'Log in' : 'Sign up'}
           </Typography>
           {!signIn && (
             <div className={classes.nameFields}>
-              <TextField
-                style={{ marginRight: 6 }}
-                className={classes.textField}
-                variant="outlined"
-                name="FirstName"
-                placeholder="First Name"
+              <Controller
+                name="firstName"
+                control={control}
+                defaultValue=""
+                rules={{ required: 'First name required' }}
+                render={({
+                  field: { onChange, value },
+                  fieldState: { error },
+                }) => (
+                  <TextField
+                    style={{ marginRight: 6 }}
+                    className={classes.textField}
+                    variant="outlined"
+                    size="small"
+                    name="firstName"
+                    id="firstName"
+                    placeholder="First Name"
+                    value={value}
+                    onChange={onChange}
+                    error={!!error}
+                    helperText={error ? error.message : null}
+                  />
+                )}
+                rules={{ required: 'First name required' }}
               />
-              <TextField
-                className={classes.textField}
-                variant="outlined"
-                name="LastName"
-                placeholder="Last Name"
+              <Controller
+                name="lastName"
+                control={control}
+                defaultValue=""
+                rules={{ required: 'Last name required' }}
+                render={({
+                  field: { onChange, value },
+                  fieldState: { error },
+                }) => (
+                  <TextField
+                    className={classes.textField}
+                    variant="outlined"
+                    size="small"
+                    name="lastName"
+                    id="lastName"
+                    placeholder="Last Name"
+                    value={value}
+                    onChange={onChange}
+                    error={!!error}
+                    helperText={error ? error.message : null}
+                  />
+                )}
+                rules={{ required: 'Last name required' }}
               />
             </div>
           )}
-          <TextField
-            className={classes.textField}
-            fullWidth
-            variant="outlined"
+          <Controller
             name="email"
-            placeholder="Email"
+            control={control}
+            defaultValue=""
+            rules={{ required: 'email required' }}
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+              <TextField
+                className={classes.textField}
+                fullWidth
+                variant="outlined"
+                size="small"
+                name="email"
+                id="email"
+                placeholder="Email"
+                value={value}
+                onChange={onChange}
+                error={!!error}
+                helperText={error ? error.message : null}
+              />
+            )}
+            rules={{ required: 'Email required' }}
           />
-          <TextField
-            className={classes.textField}
-            fullWidth
-            variant="outlined"
+          <Controller
             name="password"
-            placeholder="password"
-            type="password"
+            control={control}
+            defaultValue=""
+            rules={{ required: 'password required' }}
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+              <TextField
+                className={classes.textField}
+                fullWidth
+                variant="outlined"
+                name="password"
+                size="small"
+                id="password"
+                placeholder="password"
+                type="password"
+                value={value}
+                onChange={onChange}
+                error={!!error}
+                helperText={error ? error.message : null}
+              />
+            )}
+            rules={{ required: 'Password required' }}
           />
+
           {!signIn && (
-            <TextField
-              className={classes.textField}
-              fullWidth
-              variant="outlined"
+            <Controller
               name="confirmPassword"
-              placeholder="Confirm Password"
-              type="password"
+              control={control}
+              defaultValue=""
+              rules={{ required: 'confirm Password  required' }}
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <TextField
+                  className={classes.textField}
+                  fullWidth
+                  variant="outlined"
+                  size="small"
+                  name="confirmPassword"
+                  id="confirmPassword"
+                  placeholder="Confirm Password"
+                  type="password"
+                  value={value}
+                  onChange={onChange}
+                  error={!!error}
+                  helperText={error ? error.message : null}
+                />
+              )}
+              rules={{ required: 'Password confirm required' }}
             />
           )}
-          <Button variant="contained" fullWidth color="primary">
+          <Button type="submit" variant="contained" fullWidth color="primary">
             {signIn ? 'Log In' : 'Sign Up'}
           </Button>
           <Typography
